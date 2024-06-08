a1053beedaaa60e4edbf897f7713d3f864b9a127
Darion <daria.habibi@student.arteveldehs.be>
2024-06-08 19:50:12 +0200

    hamburger

--- clien/src/components/Header.jsx
+++ clien/src/components/Header.jsx
@@ -1,29 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { ROUTES } from "../routes/routes";
 
 function Header() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
-    <header className="flex items-center justify-between p-4 border-b">
+    <header className="flex items-center justify-between p-4 border-b bg-white dark:bg-gray-800">
       <Link to={ROUTES.home.path}>
         <img
-          src="https://placehold.co/150x50?text=Logo"
+          src="https://ahscdn.be/themes/custom/calibr8_easytheme/logo.svg?token=1716901855"
           alt="Artevelde logo"
           className="h-12"
         />
       </Link>
-      <nav>
+      <nav className="hidden md:flex items-center justify-center gap-5">
         <ul className="flex items-center justify-center gap-5">
           {Object.values(ROUTES).map((route) => (
             <li
               key={route.path}
-              className=" text-orange-300 hover:font-semibold"
+              className="text-orange-300 hover:font-semibold"
             >
               <Link to={route.path}>{route.title}</Link>
             </li>
           ))}
         </ul>
       </nav>
+      <button
+        className="md:hidden text-orange-300 focus:outline-none"
+        onClick={toggleMobileMenu}
+      >
+        <div className={` ${isMobileMenuOpen ? "open" : ""}`}>
+          <span className="block w-8 h-0.5 bg-current "></span>
+          <span className="block w-8 h-0.5 bg-current mt-1.5 "></span>
+          <span className="block w-8 h-0.5 bg-current mt-1.5 "></span>
+        </div>
+      </button>
+      {isMobileMenuOpen && (
+        <nav className="absolute top-0 left-0 w-full h-full bg-white dark:bg-gray-800 flex flex-col items-center justify-center z-10">
+          <ul className="flex flex-col items-center justify-center gap-5">
+            {Object.values(ROUTES).map((route) => (
+              <li
+                key={route.path}
+                className="text-orange-300 hover:font-semibold text-2xl"
+                onClick={toggleMobileMenu}
+              >
+                <Link to={route.path}>{route.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      )}
     </header>
   );
 }
